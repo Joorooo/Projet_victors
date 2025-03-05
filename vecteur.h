b805d6377a6a5c423a812706b686a88344da5b21
Victor Ogé <joro.victor.oge@gmail.com>
2025-03-05 21:44:27 +0100

    vecteur.h

--- vecteur.h
+++ vecteur.h
@@ -14,6 +14,7 @@ public:
     void augmente(double val);
     void set_coord(size_t indice, double valeur);
     void affiche() const;
+    void voir() const;
     bool compare(const Vecteur& v) const;
     Vecteur addition(const Vecteur& v2) const;
     Vecteur oppose() const;
