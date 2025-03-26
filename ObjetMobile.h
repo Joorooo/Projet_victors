b6b3927299b1409dbc09d873d99d8fcd8e26798b
Viktoor26 <vik.jean2126@gmail.com>
2025-03-26 18:30:11 +0100

    Add files via upload

---
+++ ObjetMobile.h
@@ -0,0 +1,28 @@
+#include "vecteur.h"
+
+class ObjetMobile {
+	
+	protected:
+	  Vecteur E;
+	  Vecteur E_prime;
+	
+	public:
+	  //constructeurs
+      ObjetMobile(const Vecteur& e, const Vecteur& e_prime);
+	  ObjetMobile(unsigned int dim);
+	  
+	  //méthodes
+	  virtual Vecteur evolution(double temps) = 0;
+	  virtual std::ostream& affiche(std::ostream& sortie) const;
+	  virtual Vecteur get_E() const;
+	  virtual Vecteur get_E_prime() const;
+	  virtual void set_E(const Vecteur& v);
+	  virtual void set_E_prime(const Vecteur& v);
+
+	  //destructeur
+	  virtual ~ObjetMobile();
+	          
+};
+
+//Opérateure externe
+std::ostream& operator<<(std::ostream& sortie, const ObjetMobile& o);
