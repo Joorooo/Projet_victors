8459696825bded42f0c17aaccc14d4986ec28787
Viktoor26 <vik.jean2126@gmail.com>
2025-03-27 10:18:20 +0100

    Update testvecteurs.cc

--- testvecteurs.cc
+++ testvecteurs.cc
@@ -2,61 +2,45 @@
 
 using namespace std;
 
-void voir(const Vecteur& v){
-		cout <<"(" ; v.affiche(); cout << ")";
-	}
-
 void test_addition(const Vecteur& v1, const Vecteur& v2) {
-	voir(v1); cout << "+"; voir(v2); cout << "="; voir(v1.addition(v2)); cout << endl;
-	voir(v2); cout << "+"; voir(v1); cout << "="; voir(v2.addition(v1)); cout << endl;
+	cout << v1 << "+" << v2 << "=" << v1+v2 << endl;
+	cout << v2 << "+" << v1 << "=" << v2 + v1 << endl << endl;
 	cout << "Testons l'addition d'un vecteur quelquonque au vecteur nul : " << endl;
-	Vecteur v_nul;
-	v_nul.augmente(0.0);
-	voir(v1); cout << "+"; voir(v_nul); cout << "="; voir(v1.addition(v_nul)); cout << endl;
+	Vecteur v_nul(3);
+	cout << v1 << "+" << v_nul << "=" << v1 + v_nul << endl;
 }
 
 void test_soustraction(const Vecteur& v1, const Vecteur& v2) {
-	voir(v1); cout << "-"; voir(v2); cout << "="; voir(v1.soustraction(v2)); cout << endl;
-	voir(v2); cout << "-"; voir(v1); cout << "="; voir(v2.soustraction(v1)); cout << endl;
+	cout << v1 << "-" << v2 << "=" << v1-v2 << endl;
+	cout << v2 << "-" << v1 << "=" << v2 - v1 << endl << endl;;
 	cout << "Testons la soustration d'un vecteur quelquonque au vecteur nul : " << endl;
-	Vecteur v_nul;
-	v_nul.augmente(0.0);
-	voir(v1); cout << "-"; voir(v_nul); cout << "="; voir(v1.soustraction(v_nul)); cout << endl;
+	Vecteur v_nul(3);
+	cout << v1 << "-" << v_nul << "=" << v1 -v_nul << endl;
 }
 	
 void test_mult(const Vecteur& v1, double x) {
-	cout << x << "*"; voir(v1); cout << "="; voir(v1.mult(x)); cout << endl;
+	cout << x << "*" << v1 << "=" << x*v1 << endl;
+	cout << v1 << "*" << x << "=" << v1*x << endl;
 }
 
 void test_normes(const Vecteur& v1) {
-	cout << "||"; voir(v1); cout << "||" << "=" ; cout << v1.norme() << endl;
-	cout << "||"; voir(v1); cout << "||^2" << "="; cout << v1.norme2() << endl;
+	cout << "||" << v1 << "||" << "=" << v1.norme() << endl;
+	cout << "||" << v1 << "||^2" << "=" << v1.norme2() << endl;
 }
 
 void test_prod_scal(const Vecteur& v1, const Vecteur& v2) {
-	voir(v1); cout << "*"; voir(v2); cout << "="; cout << v1.prod_scal(v2) << endl;
+	cout << v1 << "*" << v2 << "=" << v1 * v2 << endl;
 }
 
 
 void test_prod_vect(const Vecteur& v1, const Vecteur& v2) {
-	try {
-		Vecteur prod = v1.prod_vect(v2);
-		voir(v1); cout << "^"; voir(v2); cout << "="; voir(prod); cout << endl;
-	} catch (const invalid_argument& err) {
-		cout << "Exeption attrapée (prod_vect) : " << err.what() << endl;
-	}
-	
+		cout << v1 << "^" << v2 << "=" << (v1^v2) << endl;
 }
 
 void test_unitaire(const Vecteur& v1) {
-    try {
-        Vecteur v_unit = v1.unitaire();
-        voir(v1); cout << " normalisé devient : "; voir(v_unit); cout << endl;
+        cout << v1 << " normalisé devient : " << ~v1 << endl;
         cout << "Nous avons bien : " << endl; 
-        test_normes(v_unit);
-    } catch (const runtime_error& err) {
-        cout << "Exception attrapée (unitaire) : " << err.what() << endl;
-    }
+        test_normes(~v1);
 }
 
 void grand_test(const Vecteur& v1, const Vecteur &v2, const Vecteur &v3) {
@@ -93,4 +77,3 @@ int main() {
 	
 	return 0;
 }
-
