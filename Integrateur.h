d31ee086524e8673bb08d9c6defea28547ff95ee
Victor Ogé <joro.victor.oge@gmail.com>
2025-04-01 13:57:49 +0200

    Delete Integrateur.h

--- Integrateur.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ObjetMobile.h"
-
-class Integrateur {
-	public:
-	  virtual void integre(ObjetMobile& obj, double t, double dt) = 0;  
-	  virtual ~Integrateur() = default;
-};
-
