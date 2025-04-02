08547885e6b523e1c7559bf8057915dfba5f7d89
Victor Ogé <joro.victor.oge@gmail.com>
2025-04-02 20:20:48 +0200

    Delete ChampNewtonien.h

--- ChampNewtonien.h
+++
@@ -1,8 +0,0 @@
-#include "ForceCentrale.h"
-
-class ChampNewtonien : public ForceCentrale {
-	public:
-	
-	//methode publique
-	virtual Vecteur force(const ObjetPhysique &p, double t = 0) const;
-};
