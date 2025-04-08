bdb876dfd53c2e0334c6edf82e2098068c5c4eb8
Viktoor26 <vik.jean2126@gmail.com>
2025-04-08 14:46:33 +0200

    Add files via upload

---
+++ ChampForces.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "ChampForces.h"
+#include "ObjetPhysique.h"
+
+using namespace std;
+
+
+//Operateur d'affichage
+std::ostream& operator<<(std::ostream& sortie, const ChampForces &champ_f) {
+	return champ_f.affiche(sortie);
+}
