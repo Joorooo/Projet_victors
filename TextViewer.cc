b000808fd5b19a21d8de2e8f9be190175c7df41e
Viktoor26 <vik.jean2126@gmail.com>
2025-04-26 18:09:03 +0200

    Update TextViewer.cc

--- TextViewer.cc
+++ TextViewer.cc
@@ -12,8 +12,5 @@ void TextViewer::dessine(Systeme const& a_dessiner) {
     flot << "Dessin du Systeme : " << a_dessiner << std::endl;
 }
 
-void TextViewer::dessine_sur(PointMateriel const& a_dessiner) {
-	flot << a_dessiner.get_E() << " # position physique" << endl;
-};
 
 	
