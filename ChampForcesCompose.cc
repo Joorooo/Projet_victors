139167ba988ff24b12cd1a6adfbd56d7657b9d5f
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 10:06:38 +0200

    Update ChampForcesCompose.cc

--- ChampForcesCompose.cc
+++ ChampForcesCompose.cc
@@ -28,7 +28,7 @@ void ChampForcesCompose::ajout_champ(ChampForces* c) {
 	champs.push_back(c); //ajoute en bout de liste un pointeur sur un champ de force à attribut champs
 }
 
-//redéfinition de la méthode affiche
+//Définition de la méthode affiche
 ostream& ChampForcesCompose::affiche(ostream& sortie) const {
 	for (const auto& c : champs) {
 		 c->affiche(sortie); //appelle de manière polymorphique méthode affiche des champs de force 
