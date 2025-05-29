156ba587ab1117a3988a255684147c25e7b44eb1
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 09:36:12 +0200

    Update ForceCentrale.cc

--- ForceCentrale.cc
+++ ForceCentrale.cc
@@ -5,12 +5,11 @@ using namespace std;
 //méthode protégée
 Vecteur ForceCentrale::quadratique_inverse(const ObjetPhysique &autre_p) const {
 	if (pt_central.distance(autre_p) <= prec) {
-		Vecteur v_nul(pt_central.get_dim());
-		return v_nul;
+		return Vecteur(pt_central.get_dim()); //cf constructeurs de classe Vecteur
 	} else {
 		double l(1/(pt_central.distance2(autre_p)));
-		Vecteur v(pt_central.position()-autre_p.position());
-		return ~v*l;
+		Vecteur v(pt_central.position()-autre_p.position()); //vecteur dans la direction de droite passant par les deux objets physiques (pt_central et autre_p)
+		return ~v*l; //vecteur unitaire de v multiplié par vecteur l
 	}
 }
 
