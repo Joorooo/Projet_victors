397b052581aa0df595515ba72455285390ea272d
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 14:08:13 +0200

    Update ObjetMobile.h

--- ObjetMobile.h
+++ ObjetMobile.h
@@ -16,9 +16,9 @@ class ObjetMobile {
 	  
 	//méthodes publiques
 	virtual Vecteur evolution(double t = 0) const = 0;
-	virtual std::ostream& affiche(std::ostream& sortie) const;
-	virtual std::ostream& affiche_primaire(std::ostream& sortie) const; //retourne pour l'instant la même chose que méthode affiche
-	
+	virtual std::ostream& affiche_primaire(std::ostream& sortie) const; 
+	virtual std::ostream& affiche(std::ostream& sortie) const; //retourne pour l'instant la même chose que méthode affiche_primaire
+
 	//accesseurs
 	virtual Vecteur get_E() const;
 	virtual Vecteur get_E_prime() const;
