1e58843a944ef0c161cd93e5f679d9a0d9c87fd9
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 09:19:32 +0200

    Update ChampForces.h

--- ChampForces.h
+++ ChampForces.h
@@ -2,14 +2,14 @@
 #include <iostream>
 #include "vecteur.h"
 
-class ObjetPhysique;
+class ObjetPhysique; //pré-déclaration de la classe ObjetPhysique
 
 class ChampForces {
 	public:
-	
+
 	//méthodes virtuelles pures
 	virtual Vecteur force(const ObjetPhysique &p, double t = 0) const = 0;
-	virtual std::ostream& affiche(std::ostream& sortie) const = 0;
+	virtual std::ostream& affiche(std::ostream& sortie) const = 0; //affichage d'un champ de force
 	
 	//destructeur par défaut
 	virtual ~ChampForces() = default;
