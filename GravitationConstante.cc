64da125db4380ac80c56ccab6f04d9be5facc2d2
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 19:51:28 +0200

    Update GravitationConstante.cc

--- GravitationConstante.cc
+++ GravitationConstante.cc
@@ -8,6 +8,10 @@ GravitationConstante::GravitationConstante()
 : ForceUniforme(Constantes::g)
 {}
 
+Vecteur GravitationConstante::force(const ObjetPhysique &p, double t) const {
+	return p.get_m()*intensite;
+}
+
 //définition de la méthode affiche
 ostream& GravitationConstante::affiche(ostream& sortie) const {
 	sortie << "champ de gravitation constante, intensite : " << intensite;
