52010509ef27916bc850c1f71653913079d589b5
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 10:47:49 +0200

    Update PointMaterielAngles.cc

--- PointMaterielAngles.cc
+++ PointMaterielAngles.cc
@@ -3,26 +3,26 @@
 
 using namespace std;
 
-//méthode publique
+//redéfinition de méthode affiche
 ostream& PointMaterielAngles::affiche(ostream& sortie) const {
 	sortie << "Point Materiel à vecteur d'état angulaire périodique: " << endl;
-	ObjetPhysique::affiche(sortie);
+	PointMateriel::affiche(sortie);
 	return sortie;
 }
 
 //redéfinition du modificateur du vecteur d'état E
 void PointMaterielAngles::set_E(const Vecteur &v) {
-	Vecteur e1({1, 0});
-	Vecteur e2({0, 1});
-	double nouveau_teta = remise_angle(v*e1);
-	double nouveau_phi = remise_angle(v*e2);
-	Vecteur nouveau_E({nouveau_teta, nouveau_phi});
-	E = nouveau_E;
+	double nouveau_teta = remise_angle(v.getcoord(0));
+	double nouveau_phi = remise_angle(v.getcoord(1));
+	E = Vecteur({nouveau_teta, nouveau_phi}); //modifie le vecteur d'état E pour les angles soient toujours dans l'interval [0, 2pi[
 }
 
 //méthode privée
 double PointMaterielAngles::remise_angle(double angle) const {
 	angle = fmod(angle, 2*M_PI);
+	if (angle < 0) {
+        angle += 2 * M_PI;
+	}
 	return angle;
 }
 		
