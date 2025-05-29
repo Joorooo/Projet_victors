9e623da496634bd3b0e9554fc2ec54d221b4e30f
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 10:07:07 +0200

    Update ChampNewtonien.cc

--- ChampNewtonien.cc
+++ ChampNewtonien.cc
@@ -8,7 +8,7 @@ Vecteur ChampNewtonien::force(const ObjetPhysique &p, double t) const {
 	return Constantes::G * p.get_m() * pt_central.get_m() * quadratique_inverse(p); //vecteur de la force de gravitation (Newtonienne) exercée par pt_central sur p
 }
 
-//redéfinition de la méthode affiche
+//Définition de la méthode affiche
 ostream& ChampNewtonien::affiche(ostream& sortie) const {
 	sortie << "champ newtonien, centre : " << pt_central.get_E() << ", masse : " << pt_central.get_m();
 	return sortie;
