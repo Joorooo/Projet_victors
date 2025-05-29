29cc9a0201409033095b8ac819581db586e9b875
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 09:31:17 +0200

    Update ChampForcesCompose.cc

--- ChampForcesCompose.cc
+++ ChampForcesCompose.cc
@@ -26,7 +26,7 @@ Vecteur ChampForcesCompose::force(const ObjetPhysique &p, double t) const {
 	return resultat;
 }
 void ChampForcesCompose::ajout_champ(ChampForces* c) {
-	champs.push_back(c); //ajoute en bout de liste un pointeur sur un champ de force à 
+	champs.push_back(c); //ajoute en bout de liste un pointeur sur un champ de force à attribut champs
 }
 
 //redéfinition de la méthode affiche
