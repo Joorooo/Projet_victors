31192de41de8dc5f46fa7a8ccac49a64240a1e73
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 13:21:18 +0200

    Update vecteur.h

--- vecteur.h
+++ vecteur.h
@@ -9,13 +9,13 @@ private:
     std::vector<double> vect;
     
     //méthode privée
-    bool dim(const Vecteur& V) const;
+    bool dim(const Vecteur& V) const; //vérifie si le vecteur de l'instance courante et V ont la même dimension
 
 public:
     //constructeurs
-    Vecteur(unsigned int dimension);
-    Vecteur(double a, double b, double c);
-    Vecteur(std::initializer_list<double> liste);
+    Vecteur(unsigned int dimension); //crée le vecteur nul dans espace-vectoriel de dimension dimension
+    Vecteur(double a, double b, double c); //crée un vecteur dans R^3
+    Vecteur(std::initializer_list<double> liste); //crée un vecteur de dimension quelconque 
     
     //constructeur de copie
     Vecteur(const Vecteur &autre_vecteur);
@@ -23,12 +23,12 @@ public:
     //méthodes publiques
     Vecteur() = default; // ?
     void augmente(double val);
-    void set_coord(size_t indice, double valeur);
-    std::ostream& affiche(std::ostream& sortie) const;
-    double norme2() const;
+    double getcoord(size_t indice) const; //retourne la coordonnée d'indice indice du vecteur (instance courante)
+    void set_coord(size_t indice, double valeur); //change la valeur de la coordonnée du vecteur (instance courante) pour la valeur valeur
+    double norme2() const; //norme au carré du vecteur (instance courante)
     double norme() const;
-    unsigned int dim() const;
-    double getcoord(size_t indice) const;
+    unsigned int dim() const; //retourne la dimension du vecteur (instance courante)
+    std::ostream& affiche(std::ostream& sortie) const;
     
     //opérateur interne
     Vecteur& operator+=(const Vecteur& v);
