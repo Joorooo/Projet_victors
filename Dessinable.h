16e3623c44db58d4286c649d4ad07ae6c19ae4f9
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 18:52:02 +0200

    Update Dessinable.h

--- Dessinable.h
+++ Dessinable.h
@@ -7,10 +7,8 @@ class Dessinable {
 	  
 	  virtual ~Dessinable() = default;
 	  Dessinable(Dessinable const&)            = default;
-      Dessinable& operator=(Dessinable const&) = default;
-      Dessinable(Dessinable&&)                 = default;
-      Dessinable& operator=(Dessinable&&)      = default;
- 
-      Dessinable() = default;
-	
+          Dessinable& operator=(Dessinable const&) = default;
+          Dessinable(Dessinable&&)                 = default;
+          Dessinable& operator=(Dessinable&&)      = default;
+          Dessinable() = default;
 };
