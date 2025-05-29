1da385931935b143f79a77fcd5812e107f0c2658
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 10:00:42 +0200

    Update Contraintes.h

--- Contraintes.h
+++ Contraintes.h
@@ -7,7 +7,7 @@ class ObjetPhysique; //pré-déclaration de la classe ObjetPhysique
 class Contrainte {
 	protected:
 	
-	static constexpr double prec = 1E-9; //variable de classe représentant la masse minimale que peut avoir un objet
+	static constexpr double prec = 1E-9; //variable de classe représentant la masse minimale que peut avoir un objet subissant une contrainte
 	
 	public:
 	
