42ea50fb788e44eab81812b3484f7db55110a1d0
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 10:05:54 +0200

    Update Libre.cc

--- Libre.cc
+++ Libre.cc
@@ -6,14 +6,16 @@ using namespace std;
 //méthodes publiques
 Vecteur Libre::applique_force(const ObjetPhysique &p, const Vecteur &force, double t) const {
 	if (p.get_m() <= limite) {
-		cerr << "ERREUR--> La masse ne peut pas être nulle" << endl;
+		cerr << "-->ERREUR : La masse est trop petite (<=" << limite << ")" << endl;
 		return force;
 	} else {
-		return (1/p.get_m())*force;
+		return (1/p.get_m())*force; //cette contrainte ne modifie les accélérations de l'objet physique p
 	}
 }
 Vecteur Libre::position(const ObjetPhysique &p) const {return p.get_E();}
 Vecteur Libre::vitesse(const ObjetPhysique &p) const {return p.get_E_prime();}
+
+//Définition de la méthode affiche
 ostream& Libre::affiche(ostream& sortie) const {
 	sortie << "contrainte Libre" << endl;
 	return sortie;
