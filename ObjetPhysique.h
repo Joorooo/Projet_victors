375693c898785b724883dd0cf3eaa2508cdef44e
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 10:14:40 +0200

    Update ObjetPhysique.h

--- ObjetPhysique.h
+++ ObjetPhysique.h
@@ -19,16 +19,16 @@ class ObjetPhysique : public ObjetMobile, public Dessinable {
 	//constructeur
 	ObjetPhysique(const Vecteur &e, const Vecteur &e_prime, Contrainte* c, ChampForces* champ_f, 
 	double m, double dim = 3);
-	ObjetPhysique(const Vecteur &e, const Vecteur &e_prime, double m, double dim = 3);
+	ObjetPhysique(const Vecteur &e, const Vecteur &e_prime, double m, double dim = 3); //ne prend ni de pointeur sur une contrainte ni sur un champ de force 
 	
 	//méthodes publiques
 	Vecteur force(double t = 0) const;
 	Vecteur position() const;
 	Vecteur vitesse() const;
-	virtual Vecteur evolution(double t = 0) const override;
-	virtual std::ostream& affiche(std::ostream& sortie) const override;
 	virtual double distance(const ObjetPhysique &autre_p) const;
-	virtual double distance2(const ObjetPhysique &autre_p) const;
+	virtual double distance2(const ObjetPhysique &autre_p) const; 
+        virtual Vecteur evolution(double t = 0) const override;
+	virtual std::ostream& affiche(std::ostream& sortie) const override;
 
 	//modificateurs
 	void mod_contrainte(Contrainte* cont);
