e0257e6c51abfc7925763c0d940c9b0a8ccabc49
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 19:48:55 +0200

    Update ForceUniforme.cc

--- ForceUniforme.cc
+++ ForceUniforme.cc
@@ -6,8 +6,3 @@ using namespace std;
 ForceUniforme::ForceUniforme(const Vecteur &intensite)
 :intensite(intensite)
 {}
-
-//méthodes publiques
-Vecteur ForceUniforme::force(const ObjetPhysique &p, double t) const {
-	return p.get_m()*intensite; //vecteur de la force de gravitation (uniforme) exercée par pt_central sur p
-}
