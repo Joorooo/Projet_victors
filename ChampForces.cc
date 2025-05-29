938fab7e046b17015c4aa9eedc7bb549ee7dfa23
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-29 09:44:00 +0200

    Update ChampForces.cc

--- ChampForces.cc
+++ ChampForces.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//opérateur d'affichage
+//opérateur d'affichage externe
 std::ostream& operator<<(std::ostream& sortie, const ChampForces &champ_f) {
 	return champ_f.affiche(sortie);
 }
