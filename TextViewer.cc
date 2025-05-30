63d3dfb0d6d89e42825b1649dbf75277209a169a
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-30 10:01:11 +0200

    Update TextViewer.cc

--- TextViewer.cc
+++ TextViewer.cc
@@ -10,7 +10,7 @@ void TextViewer::dessine(PointMateriel const& a_dessiner) {a_dessiner.affiche_pr
 void TextViewer::dessine(Systeme const& a_dessiner) {
     flot << "Dessin du Systeme à t = " << a_dessiner.get_temps() << " : " << endl;
     if (a_dessiner.taille_objets() > 0) {
-		for (size_t i(0); i < a_dessiner.taille_objets(); ++i) {
+		for (size_t i(0); i < a_dessiner.taille_objets(); ++i) { //itère sur tous les objets de l'attribut objets de la classe Systeme
 			flot << "Objet no " << i+1 << " : " << endl;
 			a_dessiner.get_objet(i)->dessine_sur(*this);
 		}
