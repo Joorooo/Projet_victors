1859ff0803aa923ccd5bf782f792a64fa8d7a50c
Victor Ogé <joro.victor.oge@gmail.com>
2025-05-30 09:56:09 +0200

    Update IntegrateurEulerCromer.cc

--- IntegrateurEulerCromer.cc
+++ IntegrateurEulerCromer.cc
@@ -2,5 +2,5 @@
 
 void IntegrateurEulerCromer::integre(ObjetMobile& obj, double t, double dt) {
 	obj.set_E_prime(obj.get_E_prime() + dt*obj.evolution(t));
-	obj.set_E(obj.get_E() + dt*obj.get_E_prime());
+	obj.set_E(obj.get_E() + dt*obj.get_E_prime()); //on utiliser obj.E_prime qu'on vient de modifier
 }
